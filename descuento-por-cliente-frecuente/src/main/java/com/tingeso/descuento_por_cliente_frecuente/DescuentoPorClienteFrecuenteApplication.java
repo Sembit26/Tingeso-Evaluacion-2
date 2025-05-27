e6f9e62b9e1338332cc4953bec8c9af0ffc88e46
Sebastián Del Solar <146789359+Sembit26@users.noreply.github.com>
2025-05-27 01:42:57 -0400

    Puerto estatico para pruebas

--- descuento-por-cliente-frecuente/src/main/java/com/tingeso/descuento_por_cliente_frecuente/DescuentoPorClienteFrecuenteApplication.java
+++ descuento-por-cliente-frecuente/src/main/java/com/tingeso/descuento_por_cliente_frecuente/DescuentoPorClienteFrecuenteApplication.java
@@ -2,8 +2,10 @@ package com.tingeso.descuento_por_cliente_frecuente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DescuentoPorClienteFrecuenteApplication {
 
 	public static void main(String[] args) {
