14bffd93a95bfea4528e1cb972ce262c7e0229e0
Sebastián Del Solar <146789359+Sembit26@users.noreply.github.com>
2025-05-29 22:44:53 -0400

    Configuracion nueva para micro servicio de descuento por numero de personas

--- descuento-por-personas-service/src/main/java/com/tingeso/descuento_por_personas_service/Entity/DescuentoPorPersonas.java
+++ descuento-por-personas-service/src/main/java/com/tingeso/descuento_por_personas_service/Entity/DescuentoPorPersonas.java
@@ -1,12 +1,10 @@
 package com.tingeso.descuento_por_personas_service.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
 
 @Entity
 @Data
