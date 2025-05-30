79b1d8f43a4c2228dd0357b036d031e39d9d041b
Sebastián Del Solar <146789359+Sembit26@users.noreply.github.com>
2025-05-29 23:18:32 -0400

    Cambio

--- tarifa-dias-especiales/src/main/java/com/tingeso/tarifa_dias_especiales/Entity/TarifaDiasEspeciales.java
+++ tarifa-dias-especiales/src/main/java/com/tingeso/tarifa_dias_especiales/Entity/TarifaDiasEspeciales.java
@@ -1,9 +1,6 @@
 package com.tingeso.tarifa_dias_especiales.Entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
